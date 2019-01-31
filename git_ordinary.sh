49460ef91c4eabf543e55c9baa3f16202909ae8a
roastduckcd <roart_duck@163.com>
2019-01-31 12:43:10 +0800

    一键 git 能指定文件

--- git_ordinary.sh
+++ git_ordinary.sh
@@ -1,4 +1,5 @@
 t=$(git status)
+echo "#######################################################################################"
 echo "$t"
 echo "#######################################################################################"
 /bin/echo -n "everything OK? (y/n):" 
@@ -7,14 +8,20 @@ if [ "n" == "$ans" ]; then
 	echo "###############\nshell exit by user\n###############\n"
 	exit 0 
 else
-	git add .
 	echo "#######################################################################################"
-	read -p "				typo commit message:" msg
+	read -p "files you want to track(if no type, default to all):" files
+	if [ -z "$files" ];then
+		git add .
+	else
+		git add "$files"
+	fi
+	echo "#######################################################################################"
+	read -p "typo commit message:" msg
 	git commit -m "$msg"
 	branches=$(git branch)
 	echo "#######################################################################################"
-	echo "				you own these branches:"
-	echo "						$branches"
+	echo "you own these branches:"
+	echo "$branches"
 	echo "#######################################################################################"
 	read -p "which branch you want to push:" branch
 	git push origin $branch
