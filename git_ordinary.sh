0f14e0a0c8f496b54cdc48b352e5216621a44c20
roastduckcd <roart_duck@163.com>
2019-05-31 11:04:03 +0800

    can use tag

--- git_ordinary.sh
+++ git_ordinary.sh
@@ -2,15 +2,16 @@ t=$(git status)
 echo "#######################################################################################"
 echo "$t"
 echo "#######################################################################################"
+
 /bin/echo -n "everything OK? (y/n):" 
 read ans
-if [ "n" == "$ans" ]; then
+if [ "y" != "$ans" ]; then
 	echo "###############\nshell exit by user\n###############\n"
 	exit 0 
 else
 	echo "#######################################################################################"
 	read -p "files you want to track(if no type, default to all):" files
-	if [ -z "$files" ];then
+	if [ -z "$files" ]; then
 		git add .
 	else
 		git add "$files"
@@ -18,12 +19,29 @@ else
 	echo "#######################################################################################"
 	read -p "typo commit message:" msg
 	git commit -m "$msg"
-	branches=$(git branch)
-	echo "#######################################################################################"
-	echo "you own these branches:"
-	echo "$branches"
-	echo "#######################################################################################"
-	read -p "which branch you want to push:" branch
-	git push origin $branch
+
+    read -p "set a tag if needed:" tag
+    if [ ! -z "$tag" ]; then
+        git tag "$tag"
+    fi
+
+    read -p "ready to push?(y/n):" ready
+    if [ "y" != "$ready" ]; then
+        echo "###############\nshell exit by user\n###############\n"
+        exit 0
+    fi
+    branches=$(git branch)
+    echo "#######################################################################################"
+    echo "you own these branches:"
+    echo "$branches"
+    echo "#######################################################################################"
+    read -p "which branch you want to push:" branch
+
+    if [ ! -z "$tag" ]; then
+    git push origin $branch --tags
+    else
+    git push origin $branch
+    fi
+
 fi
 echo "shell excuted"
